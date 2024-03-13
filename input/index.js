087d5e66aa11d163981142e1b671988ca149d769
Sabardi <Sabardibahari@gmail.com>
2024-03-13 18:26:47 +0800

    Push

---
+++ input/index.js
@@ -0,0 +1,4 @@
+console.log("program dimulai");
+console.log("tampilkan text di");
+headline.innerText= "latihan fungsi output"
+console.log("done , text sudah di simpan")
---
+++ input/inputoutput.js
@@ -0,0 +1,13 @@
+        console.log("Pemerograman dimuluai");
+        console.info("Tampilan text kedalam element");
+        headline.innerText = "latihan fungsi output";
+        console.info("done, skerangan")
+
+        // menampilkan output
+        document.write("<<h2>Belajar js itu mudah dan menyenangkan </h2>>")
+        
+        console.info("website ini sudh aman");
+        console.error("haker hangan menyerang");
+        console.warn("menyerang tanpa izin adalah ilegal");
+        console.debug("isi objek headline: ",  headline);
+    
