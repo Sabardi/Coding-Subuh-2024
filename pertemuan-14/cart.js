7200b47bb11d05c09e65bf763cd6659e43550c05
Sabardi <Sabardibahari@gmail.com>
2024-03-25 04:11:53 +0800

    add

---
+++ pertemuan-14/cart.js
@@ -0,0 +1,34 @@
+class Cart {
+    constructor(){
+        this.items = [];
+    }
+
+    addItem(product){
+        if (product instanceof product) {
+            this.items.push(product);
+        }else{
+            console.error("tidak bisa menambahkan objek selain product ke keranjang")
+        }
+    }
+
+    clear(){
+        this.items = [];
+    }
+
+    count(){
+        return this.items.length;
+    }
+
+    getTotalPrice(){
+        let totalPrice = 0;
+        this.items.forEach(product => totalPrice += product.price);
+        return totalPrice;
+    }
+
+    getTotalPriceIDR(){
+        return this.getTotalPrice().toLocaleString("id",{
+            style: "currency",
+            currency: "IDR",
+        })
+    }
+}
---
+++ pertemuan-14/main.js
@@ -0,0 +1,24 @@
+// membuat objek product dan cart
+const keranjang = new Cart();
+
+// buat objek wallet dengan saldo awal
+const goPay = new Wallet(10000);
+
+// membuat objek produk
+const iPhone = new Product(
+    "iPhonr 15 pro max",
+    33000000,
+)
+
+const samsung = new Product(
+    "Samsung Galaxy A55",
+    6000000,
+    "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a55.jpg", 
+);
+
+// tambahkan iphone ke keranjang 
+keranjang.addItem(iPhone);
+
+// top up 5 jt
+goPay.topUp(5000000);
+console.log(goPay.getCurrencyBalanceIDR());
