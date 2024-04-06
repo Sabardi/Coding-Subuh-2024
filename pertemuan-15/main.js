ba113ff7aa29af72f76db80e2338a166d159a0e3
Sabardi <Sabardibahari@gmail.com>
2024-04-07 04:44:34 +0700

    membuat keranjang nya

--- pertemuan-15/main.js
+++ pertemuan-15/main.js
@@ -1,17 +1,67 @@
 // simulasi menambahkan kerjanjanh
-const keranjang = new Cart();
+const cart = new Cart();
 // membuat objek wallet
-const goPay = new Wallet(10000);
+const wallet = new Wallet(10000);
 
 // coba menambhakn product
 const iphone = new Product("iphone 15 promax", 33000000, "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15-pro-max.jpg");
 const samsung = new Product("samsung", 6000000, "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a55.jpg");
 
-// tambahkan iphone ke kerjanjang 
-keranjang.addItem(iphone);
-// keranjang.addItem(samsung);
+var product = [
+    iphone, samsung
+];
 
-// top up
 
-goPay.topUp(33000000);
-console.log(goPay.getCurrentBalanceIDR());
+// fungsi untuk handle event
+
+
+// fungsi untuk menampilkan ke elemet html
+
+function showAllProduct(){
+    const productList = document.querySelector("#product-list");
+
+    let index = 0;
+    for(roduct of products){
+        productList.innerHTML = 
+        `
+        <div class="product">
+        <img src="${product.image}" alt="">
+        <h3>${product.name}</h3>
+        <p>${product.getPriceIDR()}</p>
+        <button onclick="handleAddToCarat(${index})">Add to Cart</button>
+        </div>
+        `
+        index++;
+    }
+}
+
+// menampilkan list produk di keranjang/cart 
+function showProductInCart(){
+    const cartProductList = document.querySelector("#product-list-in-cart");
+    const totalPriceInCart = document.querySelector("#total-product-price-in-cart");
+
+    cartProductList.innerHTML = ""; // clear isi produk di keranjang
+    
+    let index = 0
+    cart.items.forEach(product => {
+        cartProductList.innerHTML += 
+        `
+        <div class="product">
+        <img src="${product.image}" alt="">
+        <div>
+            <h3>${product.name}</h3>
+            <p>${product.getPriceIDR()}</p>
+            <button onclick="handleRemoveFromCart(${index})">Hapus</button> 
+        </div>
+        </div>
+        `;
+        index++;
+    });
+
+    totalPriceInCart.textContent = cart.getTotalPriceIDR();
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+   showAllProduct(); 
+
+})
