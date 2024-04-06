3687777878e69390a43b523eb1b531683ad5ea7c
Sabardi <Sabardibahari@gmail.com>
2024-04-06 14:10:03 +0700

    membuatkan objek di main

---
+++ pertemuan-14/main.js
@@ -0,0 +1,3 @@
+// coba mengakses product
+const iphone = new Product("iphone 15 promax", 33000000, "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15-pro-max.jpg");
+const samsung = new Product("iphone samsung", 6000000, "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a55.jpg");
