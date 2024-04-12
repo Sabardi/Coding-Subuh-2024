0139c148d2645d69854f62cf78ef638276bd7a9a
Sabardi <Sabardibahari@gmail.com>
2024-04-12 21:39:53 +0700

    npx start

---
+++ pertemuan-23/vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+
+export default {
+  root: resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  }
+}
